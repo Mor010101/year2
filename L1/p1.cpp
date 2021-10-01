9fd3662c99f41906dc0af1533c6cf78f17b5f9b2
Mor010101 <tudor.morosan@student.upt.ro>
2021-10-01 16:58:01 +0300

    p1

--- L1/p1.cpp
+++ L1/p1.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-
+    char nume[20],prenume[20],adresa[100];
+    int varsta, tel;
+    cout<<"Nume: "; cin>>nume;
+    cout<<"Prenume: "; cin>>prenume;
+    cout<<"Adresa: "; cin.getline(adresa,100); cin.getline(adresa,100);  //primul getline citeste caracterul asociat lui enter
+    cout<<"Varsta: "; cin>>varsta;
+    cout<<"Telefon: "; cin>>tel;
+    cout<<"\n";
+    cout<<nume<<" "<<prenume<<"\n"<<adresa<<"\n"<<varsta<<" 0"<<tel;
     return 0;
 }
